2970c084e063cea1ffe07970458aa0440f1f309d
Konrad K Sobon <sobon.konrad@gmail.com>
2017-12-20 01:22:17 -0500

    custom text editor

---
+++ archilabUI/TextNotePlus/TextNotePlusView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xceed.Wpf.Toolkit;
+
+namespace archilabUI.TextNotePlus
+{
+    /// <summary>
+    /// Interaction logic for TextNotePlusView.xaml
+    /// </summary>
+    public partial class TextNotePlusView
+    {
+        public TextNotePlusView()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message;
+            }
+        }
+    }
+}
---
+++ archilabUI/TextNotePlus/TextNotePlusViewCustomization.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using archilabUI.ListSelector;
+using Dynamo.Controls;
+using Dynamo.Scheduler;
+using Dynamo.ViewModels;
+using Dynamo.Wpf;
+
+namespace archilabUI.TextNotePlus
+{
+    internal class TextNotePlusViewCustomization : INodeViewCustomization<TextNotePlus>
+    {
+        private DynamoViewModel _dynamoViewmodel;
+        private DispatcherSynchronizationContext _syncContext;
+        private TextNotePlus _viewModel;
+        private TextNotePlusView _view;
+
+        public void CustomizeView(TextNotePlus model, NodeView nodeView)
+        {
+            _dynamoViewmodel = nodeView.ViewModel.DynamoViewModel;
+            _syncContext = new DispatcherSynchronizationContext(nodeView.Dispatcher);
+            _viewModel = model;
+            _viewModel.EngineController = nodeView.ViewModel.DynamoViewModel.EngineController;
+            _view = new TextNotePlusView
+            {
+                DataContext = model,
+                MaxHeight = 300,
+                MaxWidth = 300
+            };
+            nodeView.inputGrid.Children.Add(_view);
+            var children = nodeView.grid.Children;
+            foreach (var child in children)
+            {
+                var e = child as FrameworkElement;
+                if(e == null) continue;
+                var name = e.Name;
+                if (name == "expansionBay") e.Visibility = Visibility.Collapsed;
+            }
+
+
+            //model.UpdateItemsCollection += UpdateCollection;
+            //UpdateCollection();
+        }
+
+        //private void UpdateCollection()
+        //{
+        //    // (Konrad) We pass a list of items already selected so that we can update them rather than create a new list.
+        //    var selected = _view._listBox.SelectedItems;
+        //    var scheduler = _dynamoViewmodel.Model.Scheduler;
+        //    var delegateBasedAsyncTask = new DelegateBasedAsyncTask(scheduler, delegate
+        //    {
+        //        _viewModel.PopulateItems(selected);
+        //    });
+        //    delegateBasedAsyncTask.ThenSend(delegate { }, _syncContext);
+        //    scheduler.ScheduleForExecution(delegateBasedAsyncTask);
+        //}
+
+        public void Dispose()
+        {
+        }
+    }
+}
