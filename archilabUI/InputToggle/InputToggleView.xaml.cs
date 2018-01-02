b5a367dcf3016ff3b38eb7e0fdc64c1ae4e8384e
Konrad K Sobon <sobon.konrad@gmail.com>
2018-01-01 23:49:38 -0500

    added input toggle node.

---
+++ archilabUI/InputToggle/InputToggleView.xaml.cs
@@ -0,0 +1,14 @@
+﻿
+namespace archilabUI.InputToggle
+{
+    /// <summary>
+    /// Interaction logic for InputToggleView.xaml
+    /// </summary>
+    public partial class InputToggleView
+    {
+        public InputToggleView()
+        {
+            InitializeComponent();
+        }
+    }
+}
