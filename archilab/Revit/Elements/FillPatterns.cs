4953d1af59ecf48dc4876975552c00bc2fe7b469
ksobon <sobon.konrad@gmail.com>
2020-01-10 23:17:33 -0500

    add fill pattern properties

--- archilab/Revit/Elements/FillPatterns.cs
+++ archilab/Revit/Elements/FillPatterns.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using Autodesk.Revit.DB;
 using Revit.Elements;
 using RevitServices.Persistence;
 using RevitServices.Transactions;
+using Element = Revit.Elements.Element;
+
 // ReSharper disable UnusedMember.Global
 
 namespace archilab.Revit.Elements
@@ -29,6 +32,126 @@ namespace archilab.Revit.Elements
                 throw new ArgumentException(nameof(fillPattern));
 
             return fpe.GetFillPattern().Target.ToString();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fillPattern"></param>
+        /// <returns></returns>
+        public static string Orientation(Element fillPattern)
+        {
+            if (fillPattern == null)
+                throw new ArgumentException(nameof(fillPattern));
+
+            if (!(fillPattern.InternalElement is Autodesk.Revit.DB.FillPatternElement fpe))
+                throw new ArgumentException(nameof(fillPattern));
+
+            var fp = fpe.GetFillPattern();
+            switch (fp.HostOrientation)
+            {
+                case FillPatternHostOrientation.ToView:
+                    return "Orient To View";
+                case FillPatternHostOrientation.AsText:
+                    return "Keep Readable";
+                case FillPatternHostOrientation.ToHost:
+                    return "Align with element";
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fillPattern"></param>
+        /// <returns></returns>
+        public static string Name(Element fillPattern)
+        {
+            if (fillPattern == null)
+                throw new ArgumentException(nameof(fillPattern));
+
+            if (!(fillPattern.InternalElement is Autodesk.Revit.DB.FillPatternElement fpe))
+                throw new ArgumentException(nameof(fillPattern));
+
+            return fpe.GetFillPattern().Name;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fillPattern"></param>
+        /// <returns></returns>
+        public static bool IsCrosshatch(Element fillPattern)
+        {
+            if (fillPattern == null)
+                throw new ArgumentException(nameof(fillPattern));
+
+            if (!(fillPattern.InternalElement is Autodesk.Revit.DB.FillPatternElement fpe))
+                throw new ArgumentException(nameof(fillPattern));
+
+            return fpe.GetFillPattern().GridCount == 2;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fillPattern"></param>
+        /// <returns></returns>
+        public static double Angle(Element fillPattern)
+        {
+            if (fillPattern == null)
+                throw new ArgumentException(nameof(fillPattern));
+
+            if (!(fillPattern.InternalElement is Autodesk.Revit.DB.FillPatternElement fpe))
+                throw new ArgumentException(nameof(fillPattern));
+
+            var fp = fpe.GetFillPattern();
+            var grids = fp.GetFillGrids();
+
+            return RadiansToDegrees(grids[0].Angle);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fillPattern"></param>
+        /// <returns></returns>
+        public static double LineSpacing1(Element fillPattern)
+        {
+            if (fillPattern == null)
+                throw new ArgumentException(nameof(fillPattern));
+
+            if (!(fillPattern.InternalElement is Autodesk.Revit.DB.FillPatternElement fpe))
+                throw new ArgumentException(nameof(fillPattern));
+
+            var fp = fpe.GetFillPattern();
+            var grids = fp.GetFillGrids();
+
+            return UnitUtils.ConvertFromInternalUnits(grids[0].Offset, DisplayUnitType.DUT_MILLIMETERS);
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fillPattern"></param>
+        /// <returns></returns>
+        public static double LineSpacing2(Element fillPattern)
+        {
+            if (fillPattern == null)
+                throw new ArgumentException(nameof(fillPattern));
+
+            if (!(fillPattern.InternalElement is Autodesk.Revit.DB.FillPatternElement fpe))
+                throw new ArgumentException(nameof(fillPattern));
+
+            var fp = fpe.GetFillPattern();
+            var count = fp.GridCount;
+            if (count < 2)
+                throw new Exception("Fill Pattern doesn't have Line spacing 2 specified.");
+
+            var grids = fp.GetFillGrids();
+
+            return UnitUtils.ConvertFromInternalUnits(grids[1].Offset, DisplayUnitType.DUT_MILLIMETERS);
         }
 
         /// <summary>
@@ -71,5 +194,19 @@ namespace archilab.Revit.Elements
 
             return fpe.ToDSType(true);
         }
+
+        #region Utilities
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="radians"></param>
+        /// <returns></returns>
+        private static double RadiansToDegrees(double radians)
+        {
+            return (radians * 180) / Math.PI;
+        }
+
+        #endregion
     }
 }
