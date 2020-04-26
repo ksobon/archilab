a8e569ce5b7cfe8884a54a51877c9757272c91c9
Konrad Sobon <sobon.konrad@gmail.com>
2020-04-25 20:17:20 -0500

    Room parsing (#59)
    * room parsing nodes
    
    * change relay behavior to use ctr key
    
    * cleanup

---
+++ archilab/Revit/Elements/ThinLinesOptions.cs
@@ -0,0 +1,25 @@
+﻿// ReSharper disable UnusedMember.Global
+
+namespace archilab.Revit.Elements
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ThinLinesOptions
+    {
+        internal ThinLinesOptions()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="show"></param>
+        public static bool ShowThinLines(bool show = true)
+        {
+            Autodesk.Revit.UI.ThinLinesOptions.AreThinLinesEnabled = show;
+
+            return show;
+        }
+    }
+}
--- archilab/Revit/Utils/DoubleExtensions.cs
+++ archilab/Revit/Utils/DoubleExtensions.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autodesk.DesignScript.Runtime;
 
 namespace archilab.Revit.Utils
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    [SupressImportIntoVM]
     public static class DoubleExtensions
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value1"></param>
+        /// <param name="value2"></param>
+        /// <returns></returns>
         public static bool AlmostEqualTo(this double value1, double value2)
         {
             return Math.Abs(value1 - value2) < 0.001;
