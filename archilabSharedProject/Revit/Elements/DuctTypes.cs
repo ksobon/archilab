b075c42fe547bf21e5ba31e06b33aa1700b6bad7
Konrad Sobon <sobon.konrad@gmail.com>
2023-07-24 14:49:05 -0500

    update rooms

---
+++ archilabSharedProject/Revit/Elements/DuctTypes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Dynamo.Graph.Nodes;
+using Revit.Elements;
+// ReSharper disable UnusedMember.Global
+
+namespace archilab.Revit.Elements
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DuctTypes
+    {
+        internal DuctTypes()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ductType"></param>
+        /// <returns></returns>
+        [NodeCategory("Query")]
+        public static string Shape(Element ductType)
+        {
+            if (ductType == null)
+                throw new ArgumentNullException(nameof(ductType));
+
+            if (!(ductType.InternalElement is Autodesk.Revit.DB.Mechanical.DuctType e))
+                throw new ArgumentException("Provided element is not a DuctType.");
+
+            return e.Shape.ToString();
+        }
+    }
+}
